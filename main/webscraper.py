59fb63042a743d339cc81643700ed0752eaf727b
Nirmal Patel <patelnirmal1325@gmail.com>
2021-12-31 00:39:38 -0500

    updated webscraper.py

--- main/webscraper.py
+++ main/webscraper.py
@@ -1,2 +1,10 @@
 #webscraper.py is used to scrape the content of various websites 
-#and store the data in the MySQL database 
+#and store the data in the MySQL database
+
+import requests
+from bs4 import BeautifulSoup as bs 
+
+URL = "https://news.google.com/topstories?hl=en-US&gl=US&ceid=US:en"
+r = requests.get(URL)
+
+soup = bs(r.content, 'html5lib')
