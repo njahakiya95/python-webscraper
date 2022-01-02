ddde22bcdf5a083d827dd29882d4fd44a53d9c89
Nirmal Patel <patelnirmal1325@gmail.com>
2022-01-02 16:44:19 -0500

    updated weather scraper project

--- main/webscraper.py
+++ main/webscraper.py
@@ -2,8 +2,13 @@
 #and store the data in the MySQL database
 
 import requests
+import argparse
 from bs4 import BeautifulSoup as bs 
 from requests.sessions import session
+
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
+# US english
+LANGUAGE = "en-US,en;q=0.5"
 
 def weather_fetcher(url):
     session = requests.Session()
@@ -28,7 +33,7 @@ def weather_fetcher(url):
     
     for day in seven_days.findAll("div", attrs={"class": "wob_df"}):
         day_name = day.findAll("div")[0].attrs['aria-label']
-        day_weather = day.findAll("img").attrs["alt"]
+        day_weather = day.find("img").attrs["alt"]
         day_temp = day.findAll("span", {"class": "wob_t"})
         day_max = day_temp[0].text
         day_min = day_temp[2].text
@@ -36,3 +41,27 @@ def weather_fetcher(url):
     
     weather_data_dict['seven_days'] = seven_days_weather
     return weather_data_dict
+
+if __name__ == "__main__":
+    URL = "https://www.google.com/search?lr=lang_en&ie=UTF-8&q=weather"
+    url_parser = argparse.ArgumentParser(description="Extract weather data from user-input location")
+    url_parser.add_argument("location", nargs="?", help="""Location whose weather data will be extracted using Google Weather. Defaults to IP if no location provided""", default="")
+
+    args = url_parser.parse_args()
+    location = args.location 
+    URL += location
+
+    data = weather_fetcher(URL)
+    print("Weather for:", data["location"])
+    print("Now:", data["time"])
+    print(f"Temperature now: {data['current_temp']}°F")
+    print("Description:", data['current_weather'])
+    print("Precipitation:", data["precipitation"])
+    print("Humidity:", data["humidity"])
+    print("Wind:", data["wind"])
+    print("Next days:")
+    for each_day in data["seven_days"]:
+        print("="*40, each_day["name"], "="*40)
+        print("Description:", each_day["current_weather"])
+        print(f"Max temperature: {each_day['max_temp']}°F")
+        print(f"Min temperature: {each_day['min_temp']}°F")
