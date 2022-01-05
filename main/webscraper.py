5a1fb133971176e3fd5db1c731f6b7aff9b01da1
Nirmal Patel <patelnirmal1325@gmail.com>
2022-01-04 23:07:47 -0500

    added comments to webscraper.py

--- main/webscraper.py
+++ main/webscraper.py
@@ -25,16 +25,17 @@ USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 LANGUAGE = "en-US,en;q=0.5"
 
 def weather_fetcher(url):
+    """Takes as an input, a user-input URL of Google Weather at a certain location and returns a dictionary with various weather data"""
     session = requests.Session()
-    session.headers['User-Agent'] = USER_AGENT
-    session.headers['Accept-Language'] = LANGUAGE
-    session.headers['Content-Language'] = LANGUAGE
-    weather_url = session.get(url)
+    session.headers['User-Agent'] = USER_AGENT          #User-Agent retrieves and presents Web content
+    session.headers['Accept-Language'] = LANGUAGE       #Accept-Language defines the language intended for the end-user
+    session.headers['Content-Language'] = LANGUAGE      #Content-Language defines the language to be used to pull the content
+    weather_url = session.get(url)                      #Gets the user-entered URL of Google weather at a particular location 
 
-    weather_data = bs(weather_url.text, "html.parser")
+    weather_data = bs(weather_url.text, "html.parser")  #create BeautifulSoup object and assign to weather_data
 
-    weather_data_dict = {}
-    weather_data_dict['location'] = weather_data.find("div", attrs={"id": "wob_loc"}).text
+    weather_data_dict = {}  #weather_data_dict is a dict that holds the below extracted weather data from weather_data
+    weather_data_dict['location'] = weather_data.find("div", attrs={"id": "wob_loc"}).text  
     weather_data_dict['time'] = weather_data.find("div", attrs={"id": "wob_dts"}).text
     weather_data_dict['current_temp'] = weather_data.find("span", attrs={"id": "wob_tm"}).text
     weather_data_dict['current_weather'] = weather_data.find("span", attrs={"id": "wob_dc"}).text
@@ -42,38 +43,40 @@ def weather_fetcher(url):
     weather_data_dict['humidity'] = weather_data.find("span", attrs={"id": "wob_hm"}).text
     weather_data_dict['wind'] = weather_data.find("span", attrs={"id": "wob_ws"}).text
 
-    seven_days_weather = []
+    seven_days_weather = [] #seven_day_weather is an array that holds the 7 days weather data from weather_data
     seven_days = weather_data.find("div", attrs={"id": "wob_dp"})
     
-    for day in seven_days.findAll("div", attrs={"class": "wob_df"}):
-        day_name = day.findAll("div")[0].attrs['aria-label']
-        day_weather = day.find("img").attrs["alt"]
+    for day in seven_days.findAll("div", attrs={"class": "wob_df"}):    #for each day in the seven_days array, find the html tag related to the weather data for each day
+        day_name = day.findAll("div")[0].attrs['aria-label']  
+        day_weather = day.find("img").attrs["alt"]              
         day_temp = day.findAll("span", {"class": "wob_t"})
         day_max = day_temp[0].text
         day_min = day_temp[2].text
         seven_days_weather.append({"name": day_name, "current_weather": day_weather, "max_temp": day_max, "min_temp": day_min})
     
-    weather_data_dict['seven_days'] = seven_days_weather
-    return weather_data_dict
+    weather_data_dict['seven_days'] = seven_days_weather    #append seven_dats_weather to weather_data_dict 
+    return weather_data_dict    #return
 
+#Execute weatherscraper.py 
 if __name__ == "__main__":
-    URL = "https://www.google.com/search?lr=lang_en&ie=UTF-8&q=weather"
-    url_parser = argparse.ArgumentParser(description="Extract weather data from user-input location")
+    URL = "https://www.google.com/search?lr=lang_en&ie=UTF-8&q=weather"     #Google weather URL without location 
+    url_parser = argparse.ArgumentParser(description="Extract weather data from user-input location")   #create and user argparse object called url_parser  
     url_parser.add_argument("location", nargs="?", help="""Location whose weather data will be extracted using Google Weather. Defaults to IP if no location provided""", default="")
 
-    args = url_parser.parse_args()
-    location = args.location 
-    URL += location
+    args = url_parser.parse_args()  #parse user-input arguement added after weatherscraper.py 
+    location = args.location #assign user-input arguement to location
+    URL += location #add location to the end of URL
 
-    data = weather_fetcher(URL)
+    data = weather_fetcher(URL) #extract relevant weather data from weather_data_dict and print 
     print("Right now it is", data["time"], "in", data["location"])
     print("The weather is", data["current_temp"], "°F", "and", data["current_weather"])
     print("There is a ", data["precipitation"], "chance of precipitation")
     print("The humiditiy is", data["humidity"], "and the wind is", data["wind"])
-    print("\n" * 3)
-    print("The seven day forecast is:")
-    for each_day in data["seven_days"]:
+    print("\n" * 3) #empty lines 
+    print("The seven day forecast is:") #print seven days forecast
+
+    for each_day in data["seven_days"]: #loop over each_day in seven_days and print the weather, max, and min temp for each_day 
         print("="*40, each_day["name"], "="*40)
-        print("The weather forecast for", each_day["name"], "is", each_day["current_weather"])
+        print("The weather forecast for", each_day["name"], "is", each_day["current_weather"])  
         print(f"The high is: {each_day['max_temp']}°F")
         print(f"The low is: {each_day['min_temp']}°F")
