2c62af2775337c5f42e74b0606b1556c3b041270
Nirmal Patel <patelnirmal1325@gmail.com>
2022-05-09 22:34:52 -0400

    added steps in weather_storer

--- main/weather_storer.py
+++ main/weather_storer.py
@@ -28,3 +28,13 @@ from requests.sessions import session
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
 LANGUAGE = "en-US,en;q=0.5"
 
+#Weather_storer function
+"""
+First create a session object to connect to the url
+Then use beautiful soup to parse the html content and store the data in an object 
+Create a dict to store the one/seven day weather data 
+Create a SQL database to store data from the dict
+Add code to save the data from the dict to a csv file
+Add code to plot seven day weather 
+Write main and prompt user for an input location 
+"""
